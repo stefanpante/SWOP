e856538db230d98907ae7e8343711c95f1dc4b8e
Dieter <Dieter@10.43.0.153>
2013-02-20 12:32:39 +0100

    Added a test source folder for all the tests. Added two example test files. Added JUnit 4 as library. Follow the same conventions for every test please.

---
+++ test/grid/TestGrid.java
@@ -0,0 +1,34 @@
+package grid;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestGrid {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
