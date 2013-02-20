e585b50bf4493c339420102cfe05acb2b184ff0c
Stefan Pante <stefanpante@outlook.com>
2013-02-20 12:33:53 +0100

    Extra changes to the coordinate class

--- src/grid/Coordinate2D.java
+++ src/grid/Coordinate2D.java
@@ -1,8 +1,11 @@
 package grid;
 
 /**
- * Coordinate2D represents a point in 2D space, has an x-coordinate and y coordinate
+ * Coordinate2D represents a point in 2D space, 
+ * has an x-coordinate and y-coordinate
+ * 
  * @author Jonas Devlieghere, Dieter Castel, Vincent Reniers and Stefan Pante
+ * @version 0.0.1RC-alpha
  *
  */
 
@@ -12,11 +15,26 @@ public class Coordinate2D {
 	private int y;
 	
 	/**
+	 * Initialize a new coordinate with a given x- and y-coordinates
 	 * 
+	 * @param	x
+	 * 			the x-coordinate of this Coordinate2D
+	 * @param	y
+	 * 			the y-coordinate of this Coordinate2D
+	 * @post	The new x-coordinate is equal to the given x
+	 * 			|	new.getX() == x
+	 * @post	the new y-coordinate is equal to the given y
+	 * 			|	new.getY() == y
+	 * @throws 	IllegalArgumentException
+	 * 			| the given x and/or y cannot be used as
+	 * 			| x or y- coordinates of this Coordinate2d
+	 * 			| !isValidX(x) || !isValidY(y)
 	 */
-	public Coordinate2D(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public Coordinate2D(int x, int y) throws IllegalArgumentException {
+		
+		setX(x);
+		setY(y);
+		
 	}
 	
 	/**
@@ -39,7 +57,7 @@ public class Coordinate2D {
 	 * @param x1
 	 */
 	//TODO Check if valid param, do we need lower and upper bound?
-	public void setX(int x1){
+	public void setX(int x1) throws IllegalArgumentException{
 		this.x = x1;
 	}
 	
@@ -48,7 +66,7 @@ public class Coordinate2D {
 	 * @param y1
 	 */
 	//TODO check if valid param, do we need lower and upper bound?
-	public void setY(int y1){
+	public void setY(int y1) throws IllegalArgumentException{
 		this.y = y1;
 	}
 	
