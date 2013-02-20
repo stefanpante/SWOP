d0dd4e3d1e4cd75d5b9968c5edc8c49095772f40
Vincent Reniers <vincent.reniers@gmail.com>
2013-02-20 12:32:59 +0100

    Direction and orientation

---
+++ src/grid/Direction.java
@@ -0,0 +1,14 @@
+package grid;
+
+/**
+ * This is used for scenarios where the player wants to move into a certain direction
+ * or for general situations where a direction is needed.
+ * 
+ * @author vincentreniers
+ */
+public enum Direction {
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST
+}
