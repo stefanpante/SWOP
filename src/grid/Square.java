c9c84142c8575ed3dd8ea7fcff6335bf7cada4d5
Vincent Reniers <vincent.reniers@gmail.com>
2013-02-22 13:38:53 +0100

    Item and items in square methods.

--- src/grid/Square.java
+++ src/grid/Square.java
@@ -1,9 +1,54 @@
 package grid;
+
+import items.Item;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Square class
+ * 
  * @author Dieter Castel, Jonas Devlieghere, Vincent Reniers en Stefan Pante
- *
  */
 public class Square {
-
+	
+	/**
+	 * List of all the items that are currently on the square.
+	 */
+	private List<Item> items = new ArrayList<Item>();
+	
+	/**
+	 * Returns a list of all the items that are currently on the square.
+	 * These may be active or inactive.
+	 */
+	public List<Item> getItems() {
+		return items;
+	}
+	
+	/**
+	 * Checks if the square has a certain item.
+	 * 
+	 * @param item
+	 */
+	public boolean hasItem(Item item) {
+		return items.contains(item);
+	}
+	
+	/**
+	 * Check if the square has an active item.
+	 */
+	public boolean hasActiveItem() {
+		Iterator<Item> iterator = items.iterator();
+		
+		while(iterator.hasNext()) {
+			Item item = iterator.next();
+			if(item.isActive()) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
 }
--- src/items/Item.java
+++ src/items/Item.java
@@ -7,4 +7,21 @@ package items;
  */
 public class Item {
 
+	/**
+	 * This flag indicates of the item is active or inactive.
+	 * An active item cannot be picked up and for example and
+	 * may cos action when entering the square.
+	 */
+	private boolean active = false;
+	
+	/**
+	 * Is the item active or inactive.
+	 * 
+	 * @return
+	 * 	True when the item is active
+	 * 	False when the item is inactive.
+	 */
+	public boolean isActive() {
+		return this.active;
+	}
 }
