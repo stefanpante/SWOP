989d8c6c89587556d9b083de1d569b1441a238b6
Stefan Pante <stefanpante@outlook.com>
2013-02-24 22:09:35 +0100

    Sidebar, runnables tested. Next step is to create a good class

--- src/gui/Sidebar.java
+++ src/gui/Sidebar.java
@@ -14,16 +14,51 @@ import simplegui.SimpleGUI;
  */
 public class Sidebar extends SimpleGUI{
 
+	/**
+	 * The buttons in this Sidebar
+	 */
 	private ArrayList<Button> buttons;
+	
 	public Sidebar(String title, int width, int height){
 		super(title,width,height);
 		buttons = new ArrayList<Button>();
-		simplegui.Button b1 = createButton(5, 10, 150, 30, null);
-		b1.setText("button 1");
-		simplegui.Button b2 = createButton(5, 50, 150, 30, null);
-		b2.setText("button 2");
-		simplegui.Button b3 = this.createButton(5, 90, 150, 30, null);
-		b3.setText("button 3");
+		
+		
+		Runnable r1 = new Runnable(){
+
+			@Override
+			public void run() {
+				System.out.println("First button was clicked");
+				
+			}
+			
+		};
+		
+		Runnable r2 = new Runnable(){
+
+			@Override
+			public void run() {
+				System.out.println("Second button was clicked");
+				
+			}
+			
+		};
+		
+		Runnable r3 = new Runnable(){
+
+			@Override
+			public void run() {
+				System.out.println("Third button was clicked");
+				
+			}
+			
+		};
+		simplegui.Button b1 = createButton(5, 10, 150, 30, r1);
+		b1.setText("Button 1");
+		simplegui.Button b2 = createButton(5, 50, 150, 30, r2);
+		b2.setText("Button 2");
+		simplegui.Button b3 = this.createButton(5, 90, 150, 30, r3);
+		b3.setText("Button 3");
 	}
 
 	/**
