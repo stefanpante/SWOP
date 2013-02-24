e0ce33a3c9f5691311c87152380db2d55f179b93
JDevliehgere <jonas.devlieghere@student.kuleuven.be>
2013-02-24 13:26:31 +0100

    Class fucking path

---
+++ src/handlers/GameHandler.java
@@ -0,0 +1,11 @@
+package handlers;
+/**
+ * Controller/Handler which controls the game
+ * @author Dieter Castel, Jonas Devlieghere, Vincent Reniers and Stefan Pante
+ *
+ */
+public class GameHandler {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/handlers/StartGameHandler.java
@@ -0,0 +1,9 @@
+package handlers;
+/**
+ * Controller/Handler which controls the start of the game
+ * @author Dieter Castel, Jonas Devlieghere, Vincent Reniers and Stefan Pante
+ *
+ */
+public class StartGameHandler {
+
+}
