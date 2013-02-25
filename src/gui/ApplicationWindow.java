e2c4a6a9c09fe0d07d770a49065ba9575fcbba89
JDevliehgere <jonas.devlieghere@student.kuleuven.be>
2013-02-25 15:58:47 +0100

    Gie Joe Fucking Aaj

--- src/gui/ApplicationWindow.java
+++ src/gui/ApplicationWindow.java
@@ -3,21 +3,49 @@
  */
 package gui;
 
+import grid.core.Coordinate2D;
+import items.Item;
+
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.UIManager;
+import javax.swing.border.TitledBorder;
 
 /**
  * @author jonas
  *
  */
-public class ApplicationWindow {
+public class ApplicationWindow implements ActionListener, MouseListener {
 
-	private JFrame frame;
+	
 	public static final int WINDOW_WIDTH = 800;
-	public static final int WINDOW_HEIGHT = 600;
+	public static final int WINDOW_HEIGHT = 523;
 	public static final int GRID_WIDTH = 500;
+	public static final int SIDEBAR_WIDTH = WINDOW_WIDTH - GRID_WIDTH;
+	public static final int ROWS = 10;
+	public static final int COLS = 10;
+	public static final int ROW_HEIGHT = GRID_WIDTH/ROWS;
+	public static final int COL_WIDTH =  GRID_WIDTH/COLS;
+	
+	private JFrame frame;	
+	private String[] inventoryItems;
 
     public static void main(String[] args) {
         try {
+        	UIManager.setLookAndFeel("com.apple.laf.AquaLookAndFeel");
         	ApplicationWindow window = new ApplicationWindow();
             window.frame.setVisible(true);
         } catch (Exception e) {
@@ -33,12 +61,138 @@ public class ApplicationWindow {
         frame = new JFrame();
         frame.setBounds(100, 100, WINDOW_WIDTH, WINDOW_HEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
-        
-        GuiGrid xyz = new GuiGrid(GRID_WIDTH, GRID_WIDTH, 10, 10);
-        frame.add(xyz);
+		frame.getContentPane().setLayout(null);
+		frame.setTitle("Objectron");
 
+        /* GRID */
+        GuiGrid gridPanel = new GuiGrid(GRID_WIDTH, GRID_WIDTH, 10, 10);
+        gridPanel.setBounds(0, 0, GRID_WIDTH, GRID_WIDTH);
+        gridPanel.setFocusable(true);
+        gridPanel.addMouseListener(this);
+        frame.getContentPane().setBackground(Color.BLACK);
+        frame.getContentPane().add(gridPanel);
         
+        /* SIDEBAR */
+        JPanel sideBarPanel = new JPanel();
+        sideBarPanel.setBounds(GRID_WIDTH, 0, SIDEBAR_WIDTH, WINDOW_HEIGHT);
+        sideBarPanel.setLayout(null);
+        frame.getContentPane().add(sideBarPanel);
+       
+        /* Current Player */
+        JPanel currentPlayerPanel = new JPanel();
+        currentPlayerPanel.setBorder(new TitledBorder(UIManager
+				.getBorder("TitledBorder.border"), "Current Player",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
+        currentPlayerPanel.setBounds(5, 5, SIDEBAR_WIDTH-10, 50);
+        sideBarPanel.add(currentPlayerPanel);
+        
+        JLabel currentPlayerLabel = new JLabel();
+        currentPlayerLabel.setText("Speler 1");
+        currentPlayerPanel.add(currentPlayerLabel);
+        
+        /* Action */
+        JPanel playerActionPanel = new JPanel();
+        playerActionPanel.setBorder(new TitledBorder(UIManager
+				.getBorder("TitledBorder.border"), "Player Actions",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
+        playerActionPanel.setBounds(5, 55, SIDEBAR_WIDTH-10, 100);
+        sideBarPanel.add(playerActionPanel);
+        
+        /* Inventory Items */
+        JPanel inventoryPanel = new JPanel();
+        inventoryPanel.setBorder(new TitledBorder(UIManager
+				.getBorder("TitledBorder.border"), "Inventory",
+				TitledBorder.LEADING, TitledBorder.TOP, null, null));
+        inventoryPanel.setBounds(5, 155, SIDEBAR_WIDTH-10, 150);
+        inventoryPanel.setLayout(null);
+        sideBarPanel.add(inventoryPanel);
+        
+        JList list = new JList(inventoryItems);
+        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list.setLayoutOrientation(JList.VERTICAL);
+        list.setVisibleRowCount(-1);
+        list.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+            	System.out.println("click");
+            }
+        });
+        JScrollPane listScroller = new JScrollPane(list);
+        listScroller.setBounds(10,20,270,120);
+        inventoryPanel.add(listScroller);
     }
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseClicked(java.awt.event.MouseEvent)
+	 */
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		Point point = e.getPoint();
+		Coordinate2D coordinate = new Coordinate2D((int)point.getX(), (int)point.getY());
+		// TODO: Whole shebang
+		System.out.println(getGridCoordinate(coordinate));
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mousePressed(java.awt.event.MouseEvent)
+	 */
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseReleased(java.awt.event.MouseEvent)
+	 */
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseEntered(java.awt.event.MouseEvent)
+	 */
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseExited(java.awt.event.MouseEvent)
+	 */
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static Coordinate2D getGridCoordinate(Coordinate2D coordinate){
+		int newX = coordinate.getX() / COL_WIDTH;
+		int newY = coordinate.getY() / ROW_HEIGHT;
+		return new Coordinate2D(newX, newY);
+	}
+	
+	public static Coordinate2D getGuiCoordinate(Coordinate2D coordinate){
+		int newX = coordinate.getX() * COL_WIDTH;
+		int newY = coordinate.getY() * ROW_HEIGHT;
+		return new Coordinate2D(newX, newY);
+	}
+	
+	public void setInventory(ArrayList<Item> list){
+		String[] inventory = new String[list.size()];
+		for(int i = 0; i < list.size(); i++)
+			inventory[i] = list.get(i).toString();
+		this.inventoryItems = inventory;
+	}
 }
--- src/gui/GuiGrid.java
+++ src/gui/GuiGrid.java
@@ -3,18 +3,16 @@
  */
 package gui;
 
-import grid.core.Coordinate2D;
-
-
-import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
+
+import javax.swing.JPanel;
 
 /**
  * @author jonas
  *
  */
-public class GuiGrid extends Canvas {
+public class GuiGrid extends JPanel {
 	
 	int width, height, rows, cols;
 	int rowHeight, colWidth;
@@ -26,15 +24,13 @@ public class GuiGrid extends Canvas {
 		this.cols = cols;
 		this.rowHeight = height/rows;
 		this.colWidth = width/cols;
-		setBackground(Color.BLACK);
 	}
 	
-	@Override
-	public void paint(Graphics graphics){
+	public void paintComponent(Graphics graphics){
 		graphics.setColor(Color.WHITE);
-	    for (int i = 0; i <= rows; i++)
+	    for (int i = 1; i <= rows-1; i++)
 	    	graphics.drawLine(0, i * rowHeight, width, i * rowHeight);
-	    for (int i = 0; i <= cols; i++)
+	    for (int i = 1; i <= cols-1; i++)
 	    	graphics.drawLine(i * colWidth, 0, i * colWidth, height);
 	}
 }
