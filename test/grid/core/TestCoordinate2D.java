2296d13149127667eb168ead4d301e129ff82835
Vincent Reniers <vincent.reniers@gmail.com>
2013-02-27 13:46:03 +0100

    Coordinate2D fix and tests added

--- test/grid/core/TestCoordinate2D.java
+++ test/grid/core/TestCoordinate2D.java
@@ -45,8 +45,53 @@ public class TestCoordinate2D {
 		
 		c2 = new Coordinate2D(-1,1);
 		
-		assertFalse(c1.equals(c2));
+		assertFalse(c1.equals(c2));	
+	}
+	
+	@Test
+	public void TestGetNeighbour() {
+		Coordinate2D coordinate = new Coordinate2D(1,1);
 		
+		Coordinate2D neighborNorthEast = new Coordinate2D(2,2);
+		Coordinate2D neighborNorth = new Coordinate2D(1,2);
+		Coordinate2D neighborNorthWest = new Coordinate2D(0,2);
+		Coordinate2D neighborWest = new Coordinate2D(0,1);
+		Coordinate2D neighborSouthWest = new Coordinate2D(0,0);
+		Coordinate2D neighborSouth = new Coordinate2D(1,0);
+		Coordinate2D neighborSouthEast = new Coordinate2D(2,0);
+		Coordinate2D neighborEast = new Coordinate2D(2,1);
+		
+		assertTrue(neighborNorthEast.equals(coordinate.getNeighbor(Direction.NORTHEAST)));
+		assertTrue(neighborNorth.equals(coordinate.getNeighbor(Direction.NORTH)));
+		assertTrue(neighborNorthWest.equals(coordinate.getNeighbor(Direction.NORTHWEST)));
+		assertTrue(neighborWest.equals(coordinate.getNeighbor(Direction.WEST)));
+		assertTrue(neighborSouthWest.equals(coordinate.getNeighbor(Direction.SOUTHWEST)));
+		assertTrue(neighborSouth.equals(coordinate.getNeighbor(Direction.SOUTH)));
+		assertTrue(neighborSouthEast.equals(coordinate.getNeighbor(Direction.SOUTHEAST)));
+		assertTrue(neighborEast.equals(coordinate.getNeighbor(Direction.EAST)));
+	}
+	
+	@Test
+	public void TestGetNeighbourNegative() {
+		Coordinate2D coordinate = new Coordinate2D(-1,0);
+		
+		Coordinate2D neighborNorthEast = new Coordinate2D(0,1);
+		Coordinate2D neighborNorth = new Coordinate2D(-1,1); 
+		Coordinate2D neighborNorthWest = new Coordinate2D(-2,1);
+		Coordinate2D neighborWest = new Coordinate2D(-2,0); 
+		Coordinate2D neighborSouthWest = new Coordinate2D(-2,-1); 
+		Coordinate2D neighborSouth = new Coordinate2D(-1,-1); 
+		Coordinate2D neighborSouthEast = new Coordinate2D(0,-1); 
+		Coordinate2D neighborEast = new Coordinate2D(0,0); 
+		
+		assertTrue(neighborNorthEast.equals(coordinate.getNeighbor(Direction.NORTHEAST)));
+		assertTrue(neighborNorth.equals(coordinate.getNeighbor(Direction.NORTH)));
+		assertTrue(neighborNorthWest.equals(coordinate.getNeighbor(Direction.NORTHWEST)));
+		assertTrue(neighborWest.equals(coordinate.getNeighbor(Direction.WEST)));
+		assertTrue(neighborSouthWest.equals(coordinate.getNeighbor(Direction.SOUTHWEST)));
+		assertTrue(neighborSouth.equals(coordinate.getNeighbor(Direction.SOUTH)));
+		assertTrue(neighborSouthEast.equals(coordinate.getNeighbor(Direction.SOUTHEAST)));
+		assertTrue(neighborEast.equals(coordinate.getNeighbor(Direction.EAST)));
 	}
 
 }
