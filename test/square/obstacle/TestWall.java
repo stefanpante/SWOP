689ca104ac1bea8e5d8d5cd6d33ebc1b4f325272
Vincent Reniers <vincent.reniers@gmail.com>
2013-03-01 01:25:28 +0100

    Introduced extra Square tests and adjusted some methods.

--- test/square/obstacle/TestWall.java
+++ test/square/obstacle/TestWall.java
@@ -4,35 +4,11 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 
+import square.Direction;
 import square.Square;
 import square.obstacles.Wall;
 
 public class TestWall {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
-	}
-	
-	public TestWall() {
-		// TODO Auto-generated constructor stub
-	}
 	
 	/**
 	 * Test for constructing with two duplicate squares.
@@ -45,43 +21,132 @@ public class TestWall {
 	}
 	
 	/**
+	 * Test for constructing and adding unconnected squares.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testConstructingUnconnected() {
+		Square squareOne = new Square();
+		Square squareTwo = new Square();
+		
+		Wall wall = new Wall(squareOne, squareTwo);
+	}
+	
+	/**
 	 * Test for constructing and adding a duplicate square.
 	 */
 	@Test(expected=IllegalArgumentException.class)
-	public void testIllegalArgumentExceptionDuplicate() {
+	public void testConstructingWithDuplicateSquares() {
 		Square squareOne = new Square();
-		Square squareTwo = new Square();
 		
 		Wall wall = new Wall(squareOne, squareOne);
-		wall.addSquare(squareOne);
 	}
 	
 	/**
-	 * Test if validSquare for duplicate is false.
+	 * Test for adding connected squares.
 	 */
-	@Test(expected=IllegalArgumentException.class)
-	public void testValidSquareDuplicate() {
+	@Test
+	public void testConstructingConnectedSquares() {
 		Square squareOne = new Square();
 		Square squareTwo = new Square();
 		
-		Wall wall = new Wall(squareOne, squareOne);
-		
-		assertFalse(wall.isValidSquare(squareOne));
+		squareOne.setNeigbor(Direction.NORTH, squareTwo);
+		Wall wall = new Wall(squareOne, squareTwo);
 	}
 	
 	/**
-	 * Test if validSquare is true for a new square.
+	 * Test after constructing if squares are contained.
 	 */
-	public  void testValidSquareNewSquare() {
+	@Test
+	public void testContainsSquare() {
 		Square squareOne = new Square();
 		Square squareTwo = new Square();
 		
-		Wall wall = new Wall(squareOne, squareOne);
+		squareOne.setNeigbor(Direction.NORTH, squareTwo);
+		Wall wall = new Wall(squareOne, squareTwo);
+		
+		assertTrue(wall.contains(squareOne));
+		assertTrue(wall.contains(squareTwo));
+		assertFalse(wall.contains(new Square()));
+	}
+	
+	/**
+	 * Test for checking if length of wall is correct.
+	 */
+	@Test
+	public void testGetLength() {
+		Square squareOne = new Square();
+		Square squareTwo = new Square();
+		
+		squareOne.setNeigbor(Direction.NORTH, squareTwo);
+		Wall wall = new Wall(squareOne, squareTwo);
+		
+		assertEquals(wall.getLength(), 2);
 		
 		Square squareThree = new Square();
+		squareTwo.setNeigbor(Direction.NORTH, squareThree);
+		wall.addSquare(squareThree);
+		
+		assertEquals(wall.getLength(), 3);
+	}
+	
+	/**
+	 * Test for adding a square to a wall.
+	 */
+	@Test
+	public void testAddingSquare() {
+		Square squareOne = new Square();
+		Square squareTwo = new Square();
+		Square squareThree = new Square();
+		
+		squareOne.setNeigbor(Direction.NORTH, squareTwo);
+		squareTwo.setNeigbor(Direction.NORTH, squareThree);
+		
+		Wall wall = new Wall(squareOne, squareTwo);
 		
 		assertTrue(wall.isValidSquare(squareThree));
+		assertFalse(wall.isValidSquare(new Square()));
+		assertFalse(wall.contains(squareThree));
+		
+		wall.addSquare(squareThree);
+		
+		assertTrue(wall.contains(squareThree));
 	}
-
+	
+	/**
+	 * Test adding square that is not connected.
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	public void testAddingUnconnectedSquare() {
+		Square squareOne = new Square();
+		Square squareTwo = new Square();
+		Square squareThree = new Square();
+		
+		squareOne.setNeigbor(Direction.NORTH, squareTwo);
+		
+		Wall wall = new Wall(squareOne, squareTwo);
+		wall.addSquare(squareThree);
+	}
+	
+	/**
+	 * Test isValidSquare for unconnected and connected squares.
+	 */
+	@Test
+	public void testIsValidSquare(){
+		Square squareOne = new Square();
+		Square squareTwo = new Square();
+		Square squareThree = new Square();
+		
+		squareOne.setNeigbor(Direction.NORTH, squareTwo);
+		squareTwo.setNeigbor(Direction.NORTH, squareThree);
+		
+		Wall wall = new Wall(squareOne, squareTwo);
+		
+		assertTrue(wall.isValidSquare(squareThree));
+		assertFalse(wall.isValidSquare(new Square()));
+		
+		wall.addSquare(squareThree);
+		
+		assertFalse(wall.isValidSquare(squareThree));
+	}
 
 }
