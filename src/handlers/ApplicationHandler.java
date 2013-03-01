bb9c01a1ebc5bc28e8b877f59dc94b9edb538a9a
Stefan Pante <stefanpante@outlook.com>
2013-03-01 12:09:58 +0100

    kommeet

--- src/handlers/ApplicationHandler.java
+++ src/handlers/ApplicationHandler.java
@@ -37,7 +37,10 @@ public class ApplicationHandler extends Handler {
     	this.pickUpHandler = new PickUpHandler(game);
     	this.useItemHandler = new UseItemHandler(game);
     }
-    
+    public GuiHandler getGuiHandler(){
+    	return guiHandler;
+    	
+    }
     public MoveHandler getMoveHandler(){
     	return this.moveHandler;
     }
