31054f46f59edc8a96b317b869ff3520c10571e2
Stefan Pante <stefanpante@outlook.com>
2013-03-06 18:56:51 +0100

    Added isvalidmove, changed the visibility of the fields. added comments

--- src/board/Board.java
+++ src/board/Board.java
@@ -8,8 +8,21 @@ import utils.Coordinate2D;
 
 public class Board {
 
-	HashMap<Coordinate2D, Square> squares;
-	ArrayList<Wall> walls;
+	/**
+	 * HashMap containing all the squares
+	 */
+	private HashMap<Coordinate2D, Square> squares;
+	
+	/**
+	 * List containing all the walls
+	 */
+	private ArrayList<Wall> walls;
+	
+	/**
+	 * Constructs a new Board with the given dimensions.
+	 * @param hSize the horizontal size of the board
+	 * @param vSize	the vertical size of the board
+	 */
 	public Board(int hSize, int vSize) {
 		BoardBuilder builder = new BoardBuilder(hSize, vSize);
 		this.squares = builder.getSquares();
@@ -17,12 +30,28 @@ public class Board {
 		
 	}
 	
+	/**
+	 * Returns all the walls on the board.
+	 * @return the walls on the board.
+	 */
 	public ArrayList<Wall> getWalls(){
 		return walls;
 	}
 	
+	/**
+	 * Returns all the squares of which the board consists.
+	 * @return the squares of the board.
+	 */
 	public HashMap<Coordinate2D, Square> getSquares(){
 		return squares;
+	}
+	
+	/**
+	 * Checks if a move to the specified coordinate is valid.
+	 * @param coordinate the coordinate which needs to be checked
+	 */
+	public boolean  isValidMove(Coordinate2D coordinate){
+		return squares.get(coordinate).isObstructed();
 	}
 
 }
