5247e63dc8852427f4417087bca4c6fe70cefa4b
dietercastel <dietercastel@gmail>
2013-03-11 21:53:16 +0100

    Added couple of more tests for wall (and altered a few according to the new implementation). Added TestItem Junit test case.

--- test/square/obstacle/TestWall.java
+++ test/square/obstacle/TestWall.java
@@ -2,6 +2,8 @@ package square.obstacle;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.*;
 
 import square.Direction;
@@ -24,11 +26,11 @@ public class TestWall {
 	 * Test for constructing and adding unconnected squares.
 	 */
 	@Test(expected=IllegalArgumentException.class)
-	public void testConstructingUnconnected() {
+	public void testConstructingOneNull() {
 		Square squareOne = new Square();
 		Square squareTwo = new Square();
 		
-		Wall wall = new Wall(squareOne, squareTwo);
+		Wall wall = new Wall(squareOne, null);
 	}
 	
 	/**
@@ -37,7 +39,6 @@ public class TestWall {
 	@Test(expected=IllegalArgumentException.class)
 	public void testConstructingWithDuplicateSquares() {
 		Square squareOne = new Square();
-		
 		Wall wall = new Wall(squareOne, squareOne);
 	}
 	
@@ -45,12 +46,15 @@ public class TestWall {
 	 * Test for adding connected squares.
 	 */
 	@Test
-	public void testConstructingConnectedSquares() {
+	public void testConstructingSquares() {
 		Square squareOne = new Square();
 		Square squareTwo = new Square();
 		
-		squareOne.setNeighbor(Direction.NORTH, squareTwo);
 		Wall wall = new Wall(squareOne, squareTwo);
+		assertTrue(squareOne.isObstructed());
+		assertTrue(squareTwo.isObstructed());
+		assertTrue(wall.equals(squareOne.getObstacle()));
+		assertTrue(wall.equals(squareTwo.getObstacle()));
 	}
 	
 	/**
@@ -61,7 +65,6 @@ public class TestWall {
 		Square squareOne = new Square();
 		Square squareTwo = new Square();
 		
-		squareOne.setNeighbor(Direction.NORTH, squareTwo);
 		Wall wall = new Wall(squareOne, squareTwo);
 		
 		assertTrue(wall.contains(squareOne));
@@ -77,13 +80,11 @@ public class TestWall {
 		Square squareOne = new Square();
 		Square squareTwo = new Square();
 		
-		squareOne.setNeighbor(Direction.NORTH, squareTwo);
 		Wall wall = new Wall(squareOne, squareTwo);
 		
 		assertEquals(wall.getLength(), 2);
 		
 		Square squareThree = new Square();
-		squareTwo.setNeighbor(Direction.NORTH, squareThree);
 		wall.addSquare(squareThree);
 		
 		assertEquals(wall.getLength(), 3);
@@ -97,34 +98,16 @@ public class TestWall {
 		Square squareOne = new Square();
 		Square squareTwo = new Square();
 		Square squareThree = new Square();
-		
-		squareOne.setNeighbor(Direction.NORTH, squareTwo);
-		squareTwo.setNeighbor(Direction.NORTH, squareThree);
-		
+				
 		Wall wall = new Wall(squareOne, squareTwo);
 		
 		assertTrue(wall.isValidSquare(squareThree));
-		assertFalse(wall.isValidSquare(new Square()));
+		assertTrue(wall.isValidSquare(new Square()));
 		assertFalse(wall.contains(squareThree));
 		
 		wall.addSquare(squareThree);
 		
 		assertTrue(wall.contains(squareThree));
-	}
-	
-	/**
-	 * Test adding square that is not connected.
-	 */
-	@Test(expected=IllegalArgumentException.class)
-	public void testAddingUnconnectedSquare() {
-		Square squareOne = new Square();
-		Square squareTwo = new Square();
-		Square squareThree = new Square();
-		
-		squareOne.setNeighbor(Direction.NORTH, squareTwo);
-		
-		Wall wall = new Wall(squareOne, squareTwo);
-		wall.addSquare(squareThree);
 	}
 	
 	/**
@@ -136,17 +119,74 @@ public class TestWall {
 		Square squareTwo = new Square();
 		Square squareThree = new Square();
 		
-		squareOne.setNeighbor(Direction.NORTH, squareTwo);
-		squareTwo.setNeighbor(Direction.NORTH, squareThree);
-		
 		Wall wall = new Wall(squareOne, squareTwo);
 		
+		assertFalse(wall.isValidSquare(squareTwo));
+		assertFalse(wall.isValidSquare(null));
 		assertTrue(wall.isValidSquare(squareThree));
-		assertFalse(wall.isValidSquare(new Square()));
 		
 		wall.addSquare(squareThree);
 		
 		assertFalse(wall.isValidSquare(squareThree));
 	}
-
+	
+	/**
+	 * Test adding a bunch of squares at once.
+	 */
+	@Test
+	public void testConstructorSquareList(){
+		Square square1 = new Square();
+		Square square2 = new Square();
+		Square square3 = new Square();
+		Square square4 = new Square();
+		
+		ArrayList<Square> list = new ArrayList<Square>();
+		
+		list.add(square1);
+		list.add(square2);
+		list.add(square3);
+		list.add(square4);
+		
+		Wall w = new Wall(list);
+		
+		assertTrue(w.contains(square1));
+		assertTrue(w.contains(square2));
+		assertTrue(w.contains(square3));
+		assertTrue(w.contains(square4));
+		assertFalse(w.contains(null));
+		assertFalse(w.contains(new Square()));
+		assertTrue(w.equals(square1.getObstacle()));
+		assertTrue(w.equals(square2.getObstacle()));
+		assertTrue(w.equals(square3.getObstacle()));
+		assertTrue(w.equals(square4.getObstacle()));
+		assertFalse(w.equals((new Square().getObstacle())));
+		assertTrue(square1.isObstructed());
+		assertTrue(square2.isObstructed());
+		assertTrue(square3.isObstructed());
+		assertTrue(square4.isObstructed());
+	}
+	
+	
+	/**
+	 * Test adding a bunch of squares at once.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstructorSquareList2(){
+		Square square1 = new Square();
+		Square square2 = new Square();
+		Square square3 = new Square();
+		Square square4 = new Square();
+		
+		ArrayList<Square> list = new ArrayList<Square>();
+		
+		list.add(square1);
+		list.add(square2);
+		list.add(square3);
+		list.add(square4);
+		//Double added should fail!!!
+		list.add(square4);
+		list.add(null);
+		
+		Wall w = new Wall(list);
+	}
 }
