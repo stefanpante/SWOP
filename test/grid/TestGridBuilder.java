384e087896de6080b2522b5b079da95298d573ac
dietercastel <dietercastel@gmail>
2013-03-11 17:10:40 +0100

    Added basic test to GridBuilder. Still work in progress. In Direction and Obstacle no actual changes (Git acting up...)

--- test/grid/TestGridBuilder.java
+++ test/grid/TestGridBuilder.java
@@ -5,12 +5,36 @@ package grid;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import utils.Coordinate;
+import utils.Coordinate2D;
 
 /**
  * @author jonas
  *
  */
 public class TestGridBuilder {
-
+	
+	@Test
+	public void testWalls(){
+		GridBuilder gb = new GridBuilder(100, 100);
+		gb.constructSquares();
+		gb.constructWalls();
+		Grid grid = gb.getGrid();
+		ArrayList<Coordinate> wallsPos = gb.getWalls();
+		Coordinate coor;
+		for(int x = 0; x < grid.getHSize(); x++){
+			for(int y = 0; y < grid.getVSize(); y++){
+				coor = new Coordinate(x, y);
+				if(wallsPos.contains(coor)){
+					assertTrue(grid.getSquare(coor).isObstructed());
+				} else {
+					assertFalse(grid.getSquare(coor).isObstructed());
+				}
+			}
+		}
+	}
 }
