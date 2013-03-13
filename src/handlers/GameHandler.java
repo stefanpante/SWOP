02f627702558abf86ff56b97a9a754589b1cc128
Vincent Reniers <vincent.reniers@gmail.com>
2013-03-13 13:40:16 +0100

    Made some code more pretty (GameHandler)

--- src/handlers/GameHandler.java
+++ src/handlers/GameHandler.java
@@ -5,9 +5,6 @@
  */
 package handlers;
 
-import items.Item;
-
-import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 
 import javax.swing.UIManager;
@@ -15,13 +12,12 @@ import javax.swing.UIManager;
 import game.Game;
 import gui.ApplicationWindow;
 import player.Player;
-import square.Direction;
 import square.Square;
 import utils.Coordinate;
 
 /**
- * @author jonas
- *
+ * 
+ * @author jonas, vreniers
  */
 public class GameHandler extends Handler {
 	
@@ -63,37 +59,45 @@ public class GameHandler extends Handler {
 			ApplicationWindow window = new ApplicationWindow(this);
 			addPropertyChangeListener(window);
 			window.setVisible();
+			
 	    	this.endTurnHandler = new EndTurnHandler(getGame(), window);
 	    	this.moveHandler = new MoveHandler(getGame(),window);
 	    	this.pickUpHandler = new PickUpHandler(getGame(),window);
 	    	this.useItemHandler = new UseItemHandler(getGame(),window);
-	    	populateGui();
-		} catch (Exception e) {
+	    	
+	    	this.populateGui();
+		} 
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 
     }
     
     /**
-	 * 
+	 * Creates a list of coordinates where grenades, walls and players are located.
+	 * This is information that the GUI can use.
 	 */
 	private void populateGui() {
     	ArrayList<Coordinate> grenades = new ArrayList<Coordinate>();
     	ArrayList<Coordinate> walls = new ArrayList<Coordinate>();
     	ArrayList<Coordinate> players = new ArrayList<Coordinate>();
+    	
     	for(Coordinate coordinate : getGame().getGrid().getAllCoordinates()){
     		Square square = getGame().getGrid().getSquare(coordinate);
+    		
     		if(square.getInventory().hasLightGrenade()){
     			grenades.add(coordinate);
     		}else if(square.isObstructed()){
     			walls.add(coordinate);
-    		}
-    			
+    		}		
     	}
+    	
     	for(Player player : getGame().getPlayers()){
     		players.add(getGame().getGrid().getCoordinate(player.getPosition()));
     	}
+    	
     	walls.removeAll(players);
+    	
     	firePropertyChange(GRENADES_PROPERTY, grenades);
     	firePropertyChange(WALLS_PROPERTY, walls);
     	firePropertyChange(PLAYERS_PROPERTY, players);
