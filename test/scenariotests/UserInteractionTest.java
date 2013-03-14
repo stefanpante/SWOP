41778add6a422c4ea581dd5829d76d92690b57c6
Stefan Pante <stefanpante@outlook.com>
2013-03-14 10:16:28 +0100

    Added userInteractionTest.

---
+++ test/scenariotests/UserInteractionTest.java
@@ -0,0 +1,51 @@
+package scenariotests;
+
+/**
+ * Tests the interaction between two users in the game
+ * 
+ * move-move-move
+ * move-move-pickup
+ * move-move-useItem
+ * move-move-endTurn
+ * 
+ * move-useItem-move
+ * move-useItem-pickup
+ * move-useItem-endTurn
+ * move-useItem-useItem
+ * 
+ * move-pickup-move
+ * move-pickup-pickup
+ * move-pickup-useItem
+ * move-pickup-endTurn
+ * 
+ * move-endTurn
+ * pickUp-endTurn
+ * useItem-endTurn
+ * 
+ * :: teveel teste?
+ * @author Stefan
+ *
+ */
+public class UserInteractionTest {
+
+	public UserInteractionTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Test switch player after three moves
+	 */
+	
+	/**
+	 * Test switch player after Move-Pickup- Move
+	 */
+
+}
