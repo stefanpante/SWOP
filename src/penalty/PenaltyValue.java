ee7ed1be5bae98a9d5a8a90a530eff9bb025460f
Stefan Pante <stefanpante@outlook.com>
2013-04-02 11:15:45 +0200

    added penaltyValue

---
+++ src/penalty/PenaltyValue.java
@@ -0,0 +1,38 @@
+package penalty;
+
+public class PenaltyValue {
+
+	/**
+	 * The number of turns lost 
+	 */
+	private int turnsLost;
+	
+	/**
+	 * the number of actions lost.
+	 */
+	private int actionsLost;
+	
+	/**
+	 * Constructs a new penaltyValue with the given parameters.
+	 * @param turnsLost		the number of turns lost.
+	 * @param actionsLost	the number of actions lost.
+	 */
+	public PenaltyValue(int turnsLost, int actionsLost) {
+		this.setActionsLost(actionsLost);
+		this.setTurnsLost(turnsLost);
+	}
+	
+	public void setActionsLost(int actionsLost){
+		
+	}
+	
+	public void setTurnsLost(int turnsLost){
+		
+	}
+	
+	public void isValidTurnsLost(){
+		
+	}
+
+
+}
