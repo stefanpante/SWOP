25c50cdc529394640e03393b2fc22a6172cbad64
JDevliehgere <jonas.devlieghere@student.kuleuven.be>
2013-04-03 15:31:11 +0200

    TurnHandler

---
+++ src/controller/TurnHandler.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package controller;
+
+import event.ActionEvent;
+import game.Game;
+
+import java.beans.PropertyChangeListener;
+import java.util.HashMap;
+import java.util.Observable;
+import java.util.Observer;
+
+import player.Player;
+
+/**
+ * @author Jonas Devlieghere
+ *
+ */
+public class TurnHandler extends Handler implements Observer {
+
+	private HashMap<Player,Integer> counter;
+	
+	public TurnHandler(Game game, PropertyChangeListener listener) {
+		super(game, listener);
+		ActionEvent.setObserver(this);
+		for(Player player : getGame().getPlayers()){
+			counter.put(player, 0);
+		}
+	}
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		if(isEndOfTurn())
+			endTurn();
+	}
+	
+	/**
+	 * Check wether this turn is finished by checking the current player
+	 * has any actions left.
+	 * 
+	 * @return	True if and only if the player has remaining actions
+	 */
+	private boolean isEndOfTurn(){
+		return getGame().getCurrentPlayer().getRemainingActions() <= 0;
+	}
+	
+	/**
+	 * End the current turn
+	 */
+	private void endTurn(){
+		if(!getGame().getCurrentPlayer().hasMoved()){
+			getGame().end();
+			throw new IllegalStateException("The current player hasn't moved in this turn " +
+					"and has no actions left and therefore lost the game");
+		}else{
+			getGame().getCurrentPlayer().endTurn();
+			getGame().switchToNextPlayer();
+		}
+	}
+	
+	/**
+	 * Start a new turn
+	 */
+	private void startTurn(){
+		increaseCurrentPlayerCount();
+		//TODO: Reset player count
+	}
+	
+	/**
+	 * Increase the turn count of the current player.
+	 */
+	private void increaseCurrentPlayerCount(){
+		Player currentPlayer = getGame().getCurrentPlayer();
+		int currentCount = counter.remove(currentPlayer);
+		counter.put(currentPlayer, ++currentCount);
+	}
+	
+
+}
