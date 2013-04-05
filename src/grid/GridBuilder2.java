4ce4af60ab3ac5184d062a35c7178b59ab8a5573
JDevliehgere <jonas.devlieghere@student.kuleuven.be>
2013-04-05 13:25:03 +0200

    More GridBuilder2

--- src/grid/GridBuilder2.java
+++ src/grid/GridBuilder2.java
@@ -3,16 +3,147 @@
  */
 package grid;
 
-import java.util.ArrayList;
+import item.Item;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import square.obstacle.Obstacle;
 import util.Coordinate;
+import be.kuleuven.cs.som.annotate.Basic;
 
 /**
  * @author jonas
  *
  */
 public class GridBuilder2 {
+	
+	/**
+	 * The grid which will be build
+	 */
+	private Grid grid;
+	
+	/**
+	 * a random generator used in various creation methods
+	 */
+	private Random random;
+	
+	/**
+	 * Creates a new Gridbuilder with parameters to create a new grid.
+	 * @param 	hSize		
+	 * 			The horizontal size of the grid this gridBuilder will build.
+	 * @param 	vSize		
+	 * 			The vertical size of the grid this gridBuilder will build.
+	 */
+	public GridBuilder2(int hSize, int vSize) {
+		setGrid(new Grid(hSize, vSize));
+		setRandom(new Random());
+	}
+	
+	/**
+	 * Returns the grid.
+	 * 
+	 * This method should only be called when the gridBuilder has done all his work.
+	 */
+	@Basic
+	protected Grid getGrid() {
+		return grid;
+	}
+	
+	/**
+	 * Sets the given grid as the gird of this gridBuilder.
+	 * 
+	 * @param grid the grid to set
+	 */
+	private void setGrid(Grid grid) {
+		this.grid = grid;
+	}
+
+	/**
+	 * Returns the random generator.
+	 * 
+	 * @return the random generator.
+	 */	
+	private Random getRandom() {
+		return random;
+	}
+
+	/**
+	 * Sets the given random as the random of this GridBuilder.
+	 * 
+	 * @param random the random to set
+	 */
+	public void setRandom(Random random) {
+		this.random = random;
+	}
+	
+	/**
+	 * Place light grenades at the the given coordinates, in respect with the given constraint.
+	 * 
+	 * @param 	coordinates
+	 * 			The coordinates where to place the light grenades
+	 * @param 	constraint
+	 * 			The constraint for placing light grenades
+	 */
 	public void placeLightGrenade(ArrayList<Coordinate> coordinates, GridConstraint constraint){
+		if(!satisfiesConstraint(coordinates, constraint))
+			throw new IllegalArgumentException("The given coordinates do not satisfy the given constraint");
+	}
+	
+	public void placeIdentityDisk(ArrayList<Coordinate> coordinates, GridConstraint constraint){
+		if(!satisfiesConstraint(coordinates, constraint))
+			throw new IllegalArgumentException("The given coordinates do not satisfy the given constraint");
 		
+	}
+	
+	public void placeWalls(ArrayList<Coordinate> coordinates, GridConstraint constraint){
+		if(!satisfiesConstraint(coordinates, constraint))
+			throw new IllegalArgumentException("The given coordinates do not satisfy the given constraint");
+		
+	}
+	
+	/**
+	 * Check whether the given list of coordinates satisfies the given constraint
+	 * 
+	 * @param 	coordinates
+	 * 			The list of coordinates to be checked
+	 * @param 	constraint
+	 * 			The constraint to be satisfied
+	 * @return	True if and only if the given list satisfies the given constraint
+	 */
+	public boolean satisfiesConstraint(ArrayList<Coordinate> coordinates, GridConstraint constraint){
+		int totalSquaes = getGrid().getHSize()*getGrid().getVSize();
+		float percentage = totalSquaes / coordinates.size();
+		if(percentage > constraint.getPercentage())
+			return false;
+		for(Coordinate coordinate : coordinates){
+			if(constraint.getExcluded().contains(coordinate))
+				return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Place an item on the given coordinate
+	 * 
+	 * @param 	coordinate
+	 * 			The coordinate to place the given item on
+	 * @param 	item
+	 * 			The item to be placed on the given coordinate
+	 */
+	private void placeItem(Coordinate coordinate, Item item){
+		getGrid().getSquare(coordinate).getInventory().addItem(item);
+	}
+	
+	/**
+	 * Place an obstacle on the given coordinate
+	 * 
+	 * @param 	coordinate
+	 * 			The coordinate to place the given obstacle on
+	 * @param 	obstacle
+	 * 			The obstacle to be placed on the given coordinate
+	 */
+	private void placeObstacle(Coordinate coordinate, Obstacle obstacle){
+		getGrid().getSquare(coordinate).setObstacle(obstacle);
 	}
 }
