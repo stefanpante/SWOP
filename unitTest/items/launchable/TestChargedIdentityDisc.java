24396d15918769174a225d34a6c6a98448f518e2
vreniers <vincent.reniers@gmail.com>
2013-04-08 14:52:02 +0200

    Identity disc

---
+++ unitTest/items/launchable/TestChargedIdentityDisc.java
@@ -0,0 +1,14 @@
+package items.launchable;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestChargedIdentityDisc {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ unitTest/items/launchable/TestIdentityDisc.java
@@ -0,0 +1,14 @@
+package items.launchable;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestIdentityDisc {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
