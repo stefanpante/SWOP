c6fb33b72a312840f9156cd01ce345a7a05b30f4
vreniers <vincent.reniers@gmail.com>
2013-04-09 14:21:04 +0200

    MoveHandler: Effects on player

--- src/event/action/MoveEvent.java
+++ src/event/action/MoveEvent.java
@@ -3,6 +3,7 @@
  */
 package event.action;
 
+import event.effect.LoseActionEvent;
 import event.effect.LoseTurnEvent;
 import event.effect.TeleportEvent;
 import game.Game;
@@ -13,11 +14,17 @@ import square.Direction;
 import square.Square;
 
 /**
- * @author jonas
- *
+ * The Move event handles all the logic for a player to move from one square
+ * to the other. It takes into account all the possible constraints and effects.
+ * Such as Teleports, LightGrenades and various other items.
+ * 
+ * @author jonas, Vincent
  */
 public class MoveEvent extends ActionEvent {
 
+	/**
+	 * The direction moving to.
+	 */
 	private Direction direction;
 	
 	/**
@@ -67,30 +74,68 @@ public class MoveEvent extends ActionEvent {
 		}
 	}
 
-	//TODO: Does this handle the effect of a lightGrenade, check if the player is in the startposition of the other player
-	// to see if he has won.
+	//TODO: check if the player is in the startposition of the other player to see if he has won.
 	@Override
 	protected void afterGameEvent(){
-		/* Activate Light Grenades on square leaving square */
+		activateLightGrenade();
+		
+		executeEffects();		
+	}
+
+	/**
+	 * If there is a LightGrenade or PowerFailure there needs to be an effect on the Player.
+	 * There are 3 possible situations.
+	 * 
+	 * 1) There is a PowerFailure and <b>active</b> LightGrenade
+	 * 2) There is only a PowerFailure
+	 * 3) There is only an <b>active</b> LightGrenade
+	 * 
+	 * @effect
+	 * 1) Player loses next 4 actions
+	 * 2) Player loses his next 3 actions.
+	 * 3) Player loses his turn.
+	 * 
+	 * @post
+	 * If there was a lightGrenade it is deactivated.
+	 */
+	private void executeEffects() {
+		boolean hasNoPower = newPosition.getPower().isFailing();
+		boolean hasActiveGrenade = false;
+		
+		LightGrenade grenade = null;
+		
+		if(newPosition.getInventory().hasLightGrenade()) {
+			grenade = newPosition.getInventory().getLightGrenade();
+			hasActiveGrenade = grenade.isActive();
+		}
+		
+		if(hasActiveGrenade && hasNoPower) {
+			new LoseActionEvent(getGame(), 4).run();	
+		}else if(hasActiveGrenade)
+			new LoseActionEvent(getGame(), 3).run();
+		else if(hasNoPower)
+			new LoseTurnEvent(getGame(),getGame().getCurrentPlayer(),1,false).run();
+		
+		if(hasActiveGrenade)
+			grenade.deactivate();
+	}
+
+	/**
+	 * If the player just dropped a LightGrenade on the square he's moving from it has 
+	 * to be activated.
+	 */
+	private void activateLightGrenade() {
 		if(currentPosition.getInventory().hasLightGrenade()){
 			LightGrenade lg = currentPosition.getInventory().getLightGrenade();
 			
 			try{
 				if(lg.isDropped()){
-					System.out.println("move and is dropped");
 					lg.activate();
 				}
 			} catch (Exception exc) {
 				exc.printStackTrace();
 			}
 		}
-		
-		if(getGame().getCurrentPlayer().getPosition().getPower().isFailing()){
-			LoseTurnEvent lte = new LoseTurnEvent(getGame(),getGame().getCurrentPlayer(),1,false);
-			lte.run();
-		}
-		
-		
 	}
 
 }
