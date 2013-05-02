a47d55ea5f290b22a029d5cc687585f105ddfb94
JDevliehgere <jonas.devlieghere@student.kuleuven.be>
2013-05-02 16:28:13 +0200

    Force F'ing Fields

---
+++ src/game/ForceFieldManager.java
@@ -0,0 +1,23 @@
+package game;
+
+import grid.Grid;
+
+/**
+ * User: jonas
+ * Date: 02/05/13
+ * Time: 15:17
+ */
+public class ForceFieldManager {
+    /**
+     * The grid the power manager is working on.
+     */
+    private final Grid grid;
+
+    public ForceFieldManager(Grid grid){
+        this.grid = grid;
+    }
+
+
+
+
+}
---
+++ src/square/field/Field.java
@@ -0,0 +1,9 @@
+package square.field;
+
+/**
+ * User: jonas
+ * Date: 02/05/13
+ * Time: 16:27
+ */
+public class Field {
+}
