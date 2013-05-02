d1f4ff240a3ccd15e2b6feefee1b555aae5bb933
Vincent Reniers <vincent.reniers@gmail.com>
2013-05-02 16:22:54 +0200

    Unidirectional ForceField

--- src/item/ForceFieldGenerator.java
+++ src/item/ForceFieldGenerator.java
@@ -1,57 +1,50 @@
 package item;
 
 import game.Player;
+
 import item.inventory.PlayerInventory;
 import item.inventory.SquareInventory;
 import item.launchable.IdentityDisc;
 import move.Movable;
 
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
+import square.obstacle.ForceField;
+
+/**
+ * The ForceFieldGenerator generates a line segment between
+ * another force field generator if it is within an appropriate distance
+ * 
+ * The Generator holds a reference to the line segment.
+ * This line segment switches on and off.
+ * 
+ * @author vincentreniers
+ */
 public class ForceFieldGenerator extends Item implements Observer{
-
-    public static final int ACTIONS_ON = 2;
-    public static final int ACTIONS_OFF = 2;
-    private static final int TOTAL_ACTIONS = ACTIONS_ON + ACTIONS_OFF;
-
-	private boolean active;
-    private int counter;
-
+    
+	/**
+	 * If the generator has been dropped this is true,
+	 * otherwise false.
+	 */
+	private boolean isUsed = false;
+	
+	/**
+	 * If the item is used, the state is changed to used.
+	 */
 	@Override
-	public void acceptAddPlayerInventory(PlayerInventory plInv)
-			throws IllegalStateException {
-		plInv.addForceFieldGenerator(this);
-
+	public void notifyUse() {
+		this.isUsed = true;
 	}
-
-	@Override
-	public void acceptRemovePlayerInventory(PlayerInventory plInv)
-			throws IllegalStateException {
-		plInv.removeForceFieldGenerator(this);
-
+	
+	/**
+	 * Returns true if the item has been used,
+	 * false if the item has not been dropped yet.
+	 */
+	public boolean isUsed(){
+		return this.isUsed;
 	}
-
-	@Override
-	public void acceptAddSquareInventory(SquareInventory sqInv)
-			throws IllegalStateException {
-		sqInv.addForceFieldGenerator(this);
-	}
-
-	@Override
-	public void acceptRemoveSquareInventory(SquareInventory sqInv)
-			throws IllegalStateException {
-		sqInv.removeForceFieldGenerator(this);
-
-	}
-
-	public boolean isActive(){
-		return this.active;
-	}
-
-    private void setActive(boolean active){
-        this.active = active;
-    }
 
 	@Override
 	public boolean isSameType(Item item) {
@@ -60,10 +53,33 @@ public class ForceFieldGenerator extends Item implements Observer{
 		return false;
 	}
 	
+	/**
+	 * Checks if the item given is a force field generator.
+	 */
 	public static boolean isForceFieldGenerator(Item item){
 		if(item instanceof ForceFieldGenerator)
 			return true;
 		return false;
+	}
+
+	@Override
+	public void acceptAddPlayerInventory(PlayerInventory plInv) throws IllegalStateException {
+		plInv.addForceFieldGenerator(this);
+	}
+
+	@Override
+	public void acceptRemovePlayerInventory(PlayerInventory plInv) throws IllegalStateException {
+		plInv.removeForceFieldGenerator(this);
+	}
+
+	@Override
+	public void acceptAddSquareInventory(SquareInventory sqInv) throws IllegalStateException {
+		sqInv.addForceFieldGenerator(this);
+	}
+
+	@Override
+	public void acceptRemoveSquareInventory(SquareInventory sqInv)	throws IllegalStateException {
+		sqInv.removeForceFieldGenerator(this);
 	}
 
     @Override
@@ -78,16 +94,13 @@ public class ForceFieldGenerator extends Item implements Observer{
 
     @Override
     public void affect(IdentityDisc identityDisc) {
+    	
     }
 
-    @Override
-    public void update(Observable o, Object arg) {
-        counter++;
-        if(counter <= ACTIONS_ON)
-            setActive(true);
-        if(counter > ACTIONS_ON && counter <= ACTIONS_ON + ACTIONS_OFF)
-            setActive(false);
-        if(counter >= TOTAL_ACTIONS)
-            counter = 0;
-    }
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+    
 }
--- src/square/obstacle/ForceField.java
+++ src/square/obstacle/ForceField.java
@@ -1,23 +1,77 @@
 package square.obstacle;
 
+import item.ForceFieldGenerator;
+
 import java.util.Observable;
 import java.util.Observer;
 
 import square.Square;
 
-public class ForceField extends MultiObstacle implements Observer {
+/**
+ * Force field is a obstacle which covers multiple squares
+ * and can be turned on and off.
+ * 
+ * @author Vincent
+ */
+public class ForceField extends MultiObstacle implements Observer{
 	
 	/**
 	 * Maximum length of a Force Field.
 	 */
 	public static final int MAX_LENGTH = 4;
 	
-	private int counter;
+	/**
+	 * Amount of actions the force field is active.
+	 */
+    public static final int ACTIONS_ON = 2;
+    
+    /**
+     * Amount of actions the force field is inactive.
+     */
+    public static final int ACTIONS_OFF = 2;
+    
+    /**
+     * Current actions remaining.
+     */
+    private int remainingActions = ACTIONS_ON;
+    
+    /**
+     * Current state of the ForceField.
+     */
+    private boolean active = true;
+    
+    /**
+     * A force field must be constructed when it is located between two generators.
+     */
+    public final ForceFieldGenerator endPointOne, endPointTwo;
 	
-	public ForceField(){
-		// TODO: Observer should listen to
+    /**
+     * If instantiated the force field is active and
+     * has its remaining actions set to 2.
+     */
+	public ForceField(ForceFieldGenerator generatorOne, ForceFieldGenerator generatorTwo) {
+		super();
+		
+		if(!isValidGenerators(generatorOne, generatorTwo))
+			throw new IllegalArgumentException("The given generators are invalid.");
+		
+		this.endPointOne = generatorOne;
+		this.endPointTwo = generatorTwo;
 	}
 	
+	/**
+	 * Checks if the given generators are valid.
+	 * 
+	 * @param generatorOne
+	 * @param generatorTwo
+	 * 
+	 * @return	False	If the generators are equal.
+	 * 			True	Otherwise
+	 */
+	public static boolean isValidGenerators(ForceFieldGenerator generatorOne,	ForceFieldGenerator generatorTwo) {
+		return generatorOne != generatorTwo;
+	}
+
 	@Override
 	public void addSquare(Square square) throws IllegalArgumentException {
 		if(!isValidSquare(square))
@@ -25,11 +79,25 @@ public class ForceField extends MultiObstacle implements Observer {
 		getSquares().add(square);
 	}
 	
+	/**
+	 * Force field cannot extend its maximum length.
+	 */
 	@Override
 	public boolean isValidSquare(Square square) {
 		if(getLength() >= MAX_LENGTH)
 			return false;
+		
 		return true;
+	}
+	
+	/**
+	 * Checks if the ForceField is active.
+	 * 
+	 * @return	True	If active
+	 * 			False	If inactive
+	 */
+	public boolean isActive() {
+		return this.active;
 	}
 	
 	@Override
@@ -37,25 +105,36 @@ public class ForceField extends MultiObstacle implements Observer {
 		return false;
 	}
 	
+	/**
+	 * Turns the force field off.
+	 */
 	private void turnOff(){
-		for(Square square: getSquares()){
+		this.active = false;
+		this.remainingActions = ACTIONS_ON;
+		
+		for (Square square: getSquares())
 			square.setObstacle(null);
-		}
 	}
 	
+	/**
+	 * Turns the force field on.
+	 */
 	private void turnOn(){
-		for(Square square: getSquares()){
-			square.setObstacle(this);
-		}
-	}
-
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		if(++counter%2 == 0){
-			turnOn();
-		}else{
-			turnOff();
-		}
+		this.active = true;
+		this.remainingActions = ACTIONS_OFF;
 		
+		for (Square square: getSquares())
+			square.setObstacle(this);
+	}
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		this.remainingActions--;
+		
+		if(isActive() && this.remainingActions <= 0) 
+			this.turnOn();	
+		
+		if(!isActive() && this.remainingActions <= 0)
+			this.turnOff();
 	}
 }
