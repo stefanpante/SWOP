d66c6b3c08ecdcc6c7bbb7f16f549689688e7ff1
Vincent Reniers <vincent.reniers@gmail.com>
2013-05-03 10:06:17 +0200

    Test Field

---
+++ unitTest/square/field/TestField.java
@@ -0,0 +1,14 @@
+package square.field;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestField {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
