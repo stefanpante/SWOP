b28ea3009cffd1413869aad41bd3de174ee20871
JDevliehgere <jonas.devlieghere@student.kuleuven.be>
2013-05-03 11:43:22 +0200

    More Documentation

--- src/command/AbstractGameCommand.java
+++ src/command/AbstractGameCommand.java
@@ -14,31 +14,61 @@ import game.Game;
  */
 public abstract class AbstractGameCommand implements Command {
 
+    /**
+     * The game of this command
+     */
 	private Game game;
-	
+
+    /**
+     * Construct a Game Command
+     * @param   game
+     *          The game of this command
+     */
 	public AbstractGameCommand(Game game){
 		setGame(game);
 	}
-	
+
+    /**
+     * Set the game of this command
+     *
+     * @param   game
+     *          The game to be set
+     */
 	private void setGame(Game game){
 		this.game = game;
 	}
-	
+
+    /**
+     * Return the game of this command
+     *
+     * @return  The game
+     */
 	public Game getGame(){
 		return this.game;
 	}
 	
 	/**
-	 * Run this Game Event
+	 * Execite this command
 	 */
 	public void execute() throws Exception {
 		beforeGameCommand();
 		duringGameCommand();
 		afterGameCommand();
 	}
-	
-	protected abstract void beforeGameCommand();
+
+    /**
+     * Operations and checks to be executed before the actual command
+     */
+	protected abstract void beforeGameCommand() throws  Exception;
+
+    /**
+     * Operations and checks to be executed during the actual command
+     */
 	protected abstract void duringGameCommand() throws Exception;
+
+    /**
+     * Operations and checks to be executed after the actual command
+     */
 	protected abstract void afterGameCommand() throws Exception;
 
 	
--- src/command/action/PlayerMoveCommand.java
+++ src/command/action/PlayerMoveCommand.java
@@ -11,7 +11,6 @@ import square.Square;
  * User: Dieter
  * Date: 2/05/13
  * Time: 15:07
- * To change this template use File | Settings | File Templates.
  */
 public class PlayerMoveCommand extends MoveCommand{
 
@@ -32,10 +31,8 @@ public class PlayerMoveCommand extends MoveCommand{
 
     @Override
     protected void afterGameCommand() throws Exception {
-    	
         activateLightGrenade();
     }
-
 
     /**
      * If the player just dropped a LightGrenade on the square he's moving from it has
