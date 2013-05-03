b4f04ad2a0f0c8a7f0e6904173a1c2f9334efe94
Stefan Pante <stefanpante@outlook.com>
2013-05-03 12:03:43 +0200

    Updated gridgui

--- src/gui/GridGui.java
+++ src/gui/GridGui.java
@@ -413,7 +413,6 @@ public class GridGui extends GUIElement{
 				SquareGUI s = new SquareGUI(squareWidth, squareHeight, getPixels(coor), gui);
                 s.setColor(gui.color(255,0));
 				if(containsTeleport(it)){
-					System.out.println("Coordinate of teleport" + coor);
 					s.setShape(Shapes.teleportItem);
 				}
 				else{
