13ea0a88b2a1ec6e6de2fc63472f594a56d804e2
Stefan Pante <stefanpante@outlook.com>
2013-05-17 14:46:01 +0200

    finished test

--- unitTest/itemplacer/TestIdentityDiscPlacer.java
+++ unitTest/itemplacer/TestIdentityDiscPlacer.java
@@ -6,13 +6,16 @@ import grid.GridProvider;
 import org.junit.*;
 import util.Coordinate;
 
+import item.IdentityDisc;
+
 import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class TestIdentityDiscPlacer {
 
-    Grid g = GridProvider.getEmptyGrid();
+	public static int RUNS = 10;
+	Grid g;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -20,41 +23,61 @@ public class TestIdentityDiscPlacer {
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		
+
 	}
 
 
 	@Before
 	public void setUp() throws Exception {
-    }
+		g = GridProvider.getEmptyGrid();
+	}
 
 	@After
 	public void tearDown() throws Exception {
-		
+
 	}
 
-    @Test
-    public void testPlayerLocations(){
-        ArrayList<Player> playerList = new ArrayList<Player>();
-        Coordinate c1 = new Coordinate(0,0);
-        Player p1 = new Player(g.getSquare(c1),1);
-        playerList.add(p1);
+	/**
+	 * Tests whether there is an identityDisc in the INCLUDED RADIUS of the player.
+	 */
+	@Test
+	public void testPlayerLocations(){
+		for(int i = 0; i < 10; i++){
+			ArrayList<Player> playerList = new ArrayList<Player>();
+			ArrayList<Coordinate> playerCoordinate = new ArrayList<Coordinate>();
+			g = GridProvider.getEmptyGrid();
+			Coordinate c1 = new Coordinate(0,0);
+			playerCoordinate.add(c1);
+			Player p1 = new Player(g.getSquare(c1),1);
+			playerList.add(p1);
 
-        Coordinate c2 = new Coordinate(9,0);
-        Player p2 = new Player(g.getSquare(c2),2);
-        playerList.add(p2);
+			Coordinate c2 = new Coordinate(9,0);
+			playerCoordinate.add(c2);
+			Player p2 = new Player(g.getSquare(c2),2);
+			playerList.add(p2);
 
-        Coordinate c3 = new Coordinate(0,9);
-        Player p3 = new Player(g.getSquare(c3),3);
-        playerList.add(p3);
+			Coordinate c3 = new Coordinate(0,9);
+			playerCoordinate.add(c3);
+			Player p3 = new Player(g.getSquare(c3),3);
+			playerList.add(p3);
 
-        Coordinate c4 = new Coordinate(9,9);
-        Player p4 = new Player(g.getSquare(c4),4);
-        playerList.add(p4);
+			Coordinate c4 = new Coordinate(9,9);
+			playerCoordinate.add(c4);
+			Player p4 = new Player(g.getSquare(c4),4);
+			playerList.add(p4);
 
-        IdentityDiscPlacer idP = new IdentityDiscPlacer(g, playerList);
-        idP.placeItems();
+			IdentityDiscPlacer idP = new IdentityDiscPlacer(g, playerList);
+			idP.placeItems();
 
-        //Assert placement around startplocation etc.
-    }
+			for(Coordinate coordinate: playerCoordinate){
+				ArrayList<Coordinate> locations = idP.getSquaredLocation(coordinate, IdentityDiscPlacer.INCLUDED_RADIUS );
+				boolean contains = false;
+				for(Coordinate coor: locations){
+					contains |= g.getSquare(coor).getInventory().containsSameType(new IdentityDisc());
+				}
+			}
+		}
+	}
+
+
 }
