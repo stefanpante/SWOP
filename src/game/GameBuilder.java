cc612ee02f126318b4a1e3cd5e08039bd970f8ed
Stefan Pante <stefanpante@outlook.com>
2013-05-20 10:34:24 +0200

    Framework voor gamebuilder, subject to lots of changes

---
+++ src/game/GameBuilder.java
@@ -0,0 +1,13 @@
+package game;
+
+public abstract class GameBuilder {
+
+	public GameBuilder() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public abstract void constructPlayers();
+	public abstract void constructGrid();
+	public abstract void placeItems();
+
+}
