72f4065497ae0c51a6718bdbf2a279e6f9500ef9
Stefan Pante <stefanpante@outlook.com>
2013-05-20 15:40:46 +0200

    Refactored startNewGame and createGame, removed unnecessary code duplication

--- src/controller/GameHandler.java
+++ src/controller/GameHandler.java
@@ -2,6 +2,9 @@ package controller;
 
 import game.Game;
 import game.Player;
+import game.gamebuilder.CTFGameBuilder;
+import game.gamebuilder.GameBuilder;
+import game.gamebuilder.RaceGameBuilder;
 import grid.AbstractGridBuilder;
 import grid.FileGridBuilder;
 import grid.RandomGridBuilder;
@@ -23,7 +26,9 @@ import util.Coordinate;
  *
  */
 public class GameHandler extends Handler {
-	
+
+	public static int CTFGAMEMODE = 0;
+	public static int RACEGAMEMODE = 1;
 	/* Handlers */
 	private EndTurnHandler endTurnHandler;
 	private MoveHandler moveHandler;
@@ -31,143 +36,126 @@ public class GameHandler extends Handler {
 	private UseItemHandler useItemHandler;
 	private TurnHandler turnHandler;
 	private ThrowLaunchableHandler throwLaunchableHandler;
-	
+
 	private PropertyChangeListener gui;
-	
-	
+
+
 	public GameHandler(PropertyChangeListener gui){
 		this.gui = gui;
 	}
 
-	
-	
-    /**
-     * Used to start a new game with a given number of hCells and vCells.
-     */
-    public void startNewGame(int hCells, int vCells){
+	/**
+	 * Initializes all the used handlers.
+	 */
+	private void initHandlers(){
+		addPropertyChangeListener(gui);
+		this.endTurnHandler = new EndTurnHandler(getGame(), gui);
+		this.moveHandler = new MoveHandler(getGame(),gui);
+		this.pickUpHandler = new PickUpHandler(getGame(),gui);
+		this.useItemHandler = new UseItemHandler(getGame(),gui);
+		this.turnHandler = new TurnHandler(getGame(), gui);
+		this.throwLaunchableHandler = new ThrowLaunchableHandler(getGame(), gui);
 		try {
-			createGame(hCells,vCells);
-			initHandlers();
-		} 
-		catch (Exception e) {
+			turnHandler.startTurn();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		this.populateGui();
+	}
 
-    }
-    
-    /**
-     * Starts a new game with a given filename.
-     * Builds a grid with the given filename as source.
-     * @param filepath	the location of the gridfile
-     */
-    public void startNewGame(String filepath){
-    	try{
-    		createGame(filepath);
-    		initHandlers();
-    	}
-    	catch(Exception e){
-    		e.printStackTrace();
-    	}
-    }
-    
-    /**
-     * Initializes all the used handlers.
-     */
-    private void initHandlers(){
-    	addPropertyChangeListener(gui);
-    	this.endTurnHandler = new EndTurnHandler(getGame(), gui);
-    	this.moveHandler = new MoveHandler(getGame(),gui);
-    	this.pickUpHandler = new PickUpHandler(getGame(),gui);
-    	this.useItemHandler = new UseItemHandler(getGame(),gui);
-    	this.turnHandler = new TurnHandler(getGame(), gui);
-    	this.throwLaunchableHandler = new ThrowLaunchableHandler(getGame(), gui);
-        try {
-            turnHandler.startTurn();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        this.populateGui();
-    }
-    
-    /**
+	/**
 	 * Creates a list of coordinates where grenades, walls and players are located.
 	 * This is information that the GUI can use.
 	 */
 	public void populateGui() {
 		HashMap<String, Object> properties = getProperties();
 		HashMap<Player,Coordinate> players = getPlayerLocations();
-		
+
 		firePropertyChange(SQUARES_PROPERTY, properties.get(SQUARES_PROPERTY));
-    	firePropertyChange(WALLS_PROPERTY, properties.get(WALLS_PROPERTY));
-    	firePropertyChange(PLAYERS_PROPERTY, players);
-    	fireChanges();
+		firePropertyChange(WALLS_PROPERTY, properties.get(WALLS_PROPERTY));
+		firePropertyChange(PLAYERS_PROPERTY, players);
+		fireChanges();
 	}
 
 	/**
-     * Returns the moveHandler.
-     * @return 	the moveHandler.
-     */
-    public MoveHandler getMoveHandler(){
-    	return this.moveHandler;
-    }
-    
+	 * Returns the moveHandler.
+	 * @return 	the moveHandler.
+	 */
+	public MoveHandler getMoveHandler(){
+		return this.moveHandler;
+	}
+
 	/**
-     * Returns the turnHandler.
-     * @return 	the moveHandler.
-     */
-    public TurnHandler getTurnHandler(){
-    	return this.turnHandler;
-    }
-    
-    /**
-     * Returns the pickUpHandler.
-     * @return 	the pickUpHandler.
-     */
-    public PickUpHandler getPickupHandler(){
-    	return this.pickUpHandler;
-    }
-    
-    /**
-     * Returns the useItemHandler.
-     * @return	the useItemHandler
-     */
-    public UseItemHandler getUseItemHandler(){
-    	return this.useItemHandler;
-    }
-    
-    /**
-     * Returns the endTurnHandler.
-     * @return 	the endTurnHandler.
-     */
-    public EndTurnHandler getEndTurnHandler(){
-    	return this.endTurnHandler;
-    }
-    /**
-     * Returns the throwLaunchableHandler.
-     * @return	the throwLaunchableHandler.
-     */
-    public ThrowLaunchableHandler getThrowLaunchableHandler(){
-    	return this.throwLaunchableHandler;
-    }
-    
+	 * Returns the turnHandler.
+	 * @return 	the moveHandler.
+	 */
+	public TurnHandler getTurnHandler(){
+		return this.turnHandler;
+	}
+
+	/**
+	 * Returns the pickUpHandler.
+	 * @return 	the pickUpHandler.
+	 */
+	public PickUpHandler getPickupHandler(){
+		return this.pickUpHandler;
+	}
+
+	/**
+	 * Returns the useItemHandler.
+	 * @return	the useItemHandler
+	 */
+	public UseItemHandler getUseItemHandler(){
+		return this.useItemHandler;
+	}
+
+	/**
+	 * Returns the endTurnHandler.
+	 * @return 	the endTurnHandler.
+	 */
+	public EndTurnHandler getEndTurnHandler(){
+		return this.endTurnHandler;
+	}
+	/**
+	 * Returns the throwLaunchableHandler.
+	 * @return	the throwLaunchableHandler.
+	 */
+	public ThrowLaunchableHandler getThrowLaunchableHandler(){
+		return this.throwLaunchableHandler;
+	}
+
 	/**
 	 * @param hSize the hSize of the grid on which the game is played
 	 * @param vSize the vSize of the grid on which the game is played
 	 */
-	public void createGame(int hSize, int vSize) {
-		RandomGridBuilder builder = new RandomGridBuilder(hSize, vSize);
-		
-		setGame(new Game(builder.getGrid()));
-	}
-	
-	public void createGame(String filename){
-		FileGridBuilder builder;
-		try {
-			builder = new FileGridBuilder(filename);
-			setGame(new Game(builder.getGrid()));
-		} catch (IOException e) {
-			e.printStackTrace();
+	public void startNewGame(int hSize, int vSize, int numOfPlayers, int gameMode) {
+		GameBuilder builder;
+
+		switch(gameMode){
+		case 0:		builder = new CTFGameBuilder(hSize, vSize, numOfPlayers);
+		break;
+		case 1:		builder = new RaceGameBuilder(hSize, vSize, numOfPlayers);
+		break;
+		default: 	throw new IllegalStateException(" The specified game mode doesnt exist.");
 		}
+
+		setGame(builder.getGame());
+		initHandlers();
+	}
+
+	public void startNewGame(String filename, int numOfPlayers, int gameMode) throws IOException{
+		GameBuilder builder;
+
+		switch(gameMode){
+		case 0:		builder = new CTFGameBuilder(filename, numOfPlayers);
+		break;
+		case 1:		builder = new RaceGameBuilder(filename, numOfPlayers);
+		break;
+		default: 	throw new IllegalStateException(" The specified game mode doesnt exist.");
+		}
+
+		setGame(builder.getGame());
+		initHandlers();
 	}
 
 }
