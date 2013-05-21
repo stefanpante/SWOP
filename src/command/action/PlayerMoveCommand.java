e242f946476fb5de1ca9c9c7d3b06ddca2dbd3ed
Stefan Pante <stefanpante@outlook.com>
2013-05-21 21:15:38 +0200

    Deprecated tag for activateLightGrenade

--- src/command/action/PlayerMoveCommand.java
+++ src/command/action/PlayerMoveCommand.java
@@ -51,6 +51,8 @@ public class PlayerMoveCommand extends MoveCommand{
      * If the player just dropped a LightGrenade on the square he's moving from it has
      * to be activated.
      */
+    // XXX: Shouldn't be necessary anymore.
+    @Deprecated
     private void activateLightGrenade() {
         if(getStartPosition().hasType(new LightGrenade())){
             LightGrenade lg = getStartPosition().getType(new LightGrenade());
